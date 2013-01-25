f1bdfb5156e5fef4536e75ff3a3578c7e4e2d269
Moose W. Oler <wangjun.cumtb@ieee.org>
2013-01-26 02:56:55 +0800

    modify CMakeLists.txt. The project can be compiled by issuing build.sh 	modified:   CMakeLists.txt 	modified:   src/CMakeLists.txt

---
+++ build/arm/src/wpa/CMakeFiles/onyx_wpa.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/moose/Code/github/onyx-helloworld/src/wpa/os_unix.c" "/home/moose/Code/github/onyx-helloworld/build/arm/src/wpa/CMakeFiles/onyx_wpa.dir/os_unix.c.o"
+  "/home/moose/Code/github/onyx-helloworld/src/wpa/wpa_ctrl.c" "/home/moose/Code/github/onyx-helloworld/build/arm/src/wpa/CMakeFiles/onyx_wpa.dir/wpa_ctrl.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "BUILD_FOR_ARM"
+  "ENABLE_EINK_SCREEN"
+  "BS60_INIT_MAIN"
+  "BS80_INIT_MAIN"
+  "BS97_INIT_MAIN"
+  "SFM_M25P20"
+  "SHARE_ROOT=\"/usr/share\""
+  "CONFIG_CTRL_IFACE"
+  "CONFIG_CTRL_IFACE_UNIX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/arm/src/wpa/CMakeFiles/onyx_wpa.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+FILE(REMOVE_RECURSE
+  "../../libs/libonyx_wpa.a"
+)
