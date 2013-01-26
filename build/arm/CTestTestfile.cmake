481a17d2f90b0a6831b3a8a1bcc1edbb0295d2ca
Moose W. Oler <wangjun.cumtb@ieee.org>
2013-01-26 11:05:10 +0800

    ignore build directory.

--- build/arm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/moose/Code/github/onyx-helloworld
-# Build directory: /home/moose/Code/github/onyx-helloworld/build/arm
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src/wpa)
-SUBDIRS(src)
