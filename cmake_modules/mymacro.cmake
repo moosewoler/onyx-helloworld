b3d90f4ee3e444578929319287d3898c85b9116f
Moose W. Oler <wangjun.cumtb@ieee.org>
2013-01-28 13:00:47 +0800

    add gomoku into src. build success, cannot run on device.

--- cmake_modules/mymacro.cmake
+++ cmake_modules/mymacro.cmake
@@ -1,53 +1,33 @@
 macro       (PRE_CONFIGURE)
-# set up cmake variables
-set         (BUILD_FOR_ARM      ON)
-set         (ONYX_SDK_ROOT      "/opt/onyx/arm")
-set         (USE_CORTEX_A8      ON)
-set         (LINK_ZLIB_DEFAULT  ON)
-#enable_testing()
+    # set up cmake variables
+    set (BUILD_FOR_ARM      ON)
+    set (ONYX_SDK_ROOT      "/opt/onyx/arm")
+    set (USE_CORTEX_A8      ON)
+    set (LINK_ZLIB_DEFAULT  ON)
+    set (EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)
+    set (LIBRARY_OUTPUT_PATH    ${PROJECT_BINARY_DIR}/libs)
+    set (TEST_OUTPUT_PATH       ${PROJECT_BINARY_DIR}/unittests)
+    set (QT_PLUGINS_OUTPUT_PATH ${PROJECT_BINARY_DIR}/plugins)
+    set (ONYX_SDK_LIB_MYBUILD   "~/Code/booxsdk-master/build/arm/libs/")
+    #enable_testing()
 
-# Check to use arm toolchain or not
-IF(BUILD_FOR_ARM)
-    include (arm_toolchain)
-    USE_ARM_TOOLCHAIN()
-ENDIF(BUILD_FOR_ARM)
+    # Check to use arm toolchain or not
+    if(BUILD_FOR_ARM)
+        USE_ARM_TOOLCHAIN()
+    endif(BUILD_FOR_ARM)
 
-IF(UNIX OR BUILD_FOR_ARM)
-    ADD_DEFINITIONS(-DSHARE_ROOT="/usr/share")
-ELSE (UNIX OR BUILD_FOR_ARM)
-    ADD_DEFINITIONS(-DSHARE_ROOT="")
-    ADD_DEFINITIONS(-D_CRT_SECURE_NO_WARNINGS)
-ENDIF (UNIX OR BUILD_FOR_ARM)
+    if(UNIX OR BUILD_FOR_ARM)
+        add_definitions(-DSHARE_ROOT="/usr/share")
+    else (UNIX OR BUILD_FOR_ARM)
+        add_definitions(-DSHARE_ROOT="")
+        add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+    endif (UNIX OR BUILD_FOR_ARM)
 
-add_definitions(-DCONFIG_CTRL_IFACE)
-add_definitions(-DCONFIG_CTRL_IFACE_UNIX)
+    add_definitions(-DCONFIG_CTRL_IFACE)
+    add_definitions(-DCONFIG_CTRL_IFACE_UNIX)
 
-include     (strict_warning)
-
-# Project include directories.
-include_directories(BEFORE
-    #${CMAKE_SOURCE_DIR}/code/include
-    #${CMAKE_SOURCE_DIR}/third_party/gtest/include
-    ${CMAKE_SOURCE_DIR}/src/include
-    ${CMAKE_FIND_ROOT_PATH}/include
-)
-
-# Find thread library for the current platform
-include(FindThreads)
-include(enable_qt)
-include(qt4_wrap_ts)
-include(onyx_test)
-include(misc)
-include(tc)
-
-message (STATUS "QT_LIBRARY_DIR is set to ${QT_LIBRARY_DIR}")
-link_directories (${QT_LIBRARY_DIR})
-
-# Output directories.
-set     (EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)
-set     (LIBRARY_OUTPUT_PATH    ${PROJECT_BINARY_DIR}/libs)
-set     (TEST_OUTPUT_PATH       ${PROJECT_BINARY_DIR}/unittests)
-set     (QT_PLUGINS_OUTPUT_PATH ${PROJECT_BINARY_DIR}/plugins)
-
-
+    # Project include directories.
+    message             (STATUS "QT_LIBRARY_DIR is set to ${QT_LIBRARY_DIR}")
+    include_directories (BEFORE ${CMAKE_SOURCE_DIR}/src/include ${CMAKE_FIND_ROOT_PATH}/include)
+    link_directories    (${ONYX_SDK_LIB_MYBUILD} ${QT_LIBRARY_DIR})
 endmacro    (PRE_CONFIGURE)
